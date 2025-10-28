4fee0ac72ff71454cce13f3d7ce2dd5371aadfdc
Albert Medina <albertmecu@gmail.com>
2025-10-28 10:59:57 +0100

    refactor: minor naming tweaks

--- University/university.sql
+++ University/university.sql
@@ -282,13 +282,13 @@ SELECT nombre AS asignatura FROM asignatura WHERE cuatrimestre = 1 AND curso = 3
 SELECT per.apellido1, per.apellido2, per.nombre, d.nombre AS departamento FROM persona per JOIN profesor pro ON per.id = pro.id_profesor JOIN departamento d ON pro.id_departamento = d.id ORDER BY per.apellido1, per.apellido2, per.nombre;
 
 -- 7. Subjects names, start year and finish year of student with NIF '26902806M'
-SELECT s.nombre AS asignatura, c.anyo_inicio, c.anyo_fin FROM curso_escolar c JOIN alumno_se_matricula_asignatura a ON c.id = a.id_curso_escolar JOIN asignatura s ON a.id_asignatura = s.id JOIN persona p ON a.id_alumno = p.id WHERE p.nif = "26902806M";
+SELECT sub.nombre AS asignatura, c.anyo_inicio, c.anyo_fin FROM curso_escolar c JOIN alumno_se_matricula_asignatura stu ON c.id = stu.id_curso_escolar JOIN asignatura sub ON stu.id_asignatura = sub.id JOIN persona p ON stu.id_alumno = p.id WHERE p.nif = "26902806M";
 
 -- 8. Department names which any of its professors teach any subject in 'Grado en Ingeniería Informática (Plan 2015)'
 SELECT DISTINCT d.nombre AS departamento FROM departamento d JOIN profesor p ON d.id = p.id_departamento JOIN asignatura s ON s.id_profesor = p.id_profesor JOIN grado g ON s.id_grado = g.id WHERE g.nombre = "Grado en Ingeniería Informática (Plan 2015)";
 
 -- 9. Name and surnames of students studing any subject in 2018/2019
-SELECT DISTINCT p.nombre, p.apellido1, p.apellido2 FROM persona p JOIN alumno_se_matricula_asignatura a ON p.id = a.id_alumno JOIN curso_escolar c ON a.id_curso_escolar = c.id WHERE c.anyo_inicio = 2018 AND c.anyo_fin = 2019;
+SELECT DISTINCT p.nombre, p.apellido1, p.apellido2 FROM persona p JOIN alumno_se_matricula_asignatura s ON p.id = s.id_alumno JOIN curso_escolar c ON s.id_curso_escolar = c.id WHERE c.anyo_inicio = 2018 AND c.anyo_fin = 2019;
 
 -- -----------------------------------------------------
 -- Left/Right Join queries
@@ -310,7 +310,7 @@ SELECT per.apellido1, per.apellido2, per.nombre FROM persona per JOIN profesor p
 SELECT s.nombre AS asignatura FROM asignatura s LEFT JOIN profesor p ON s.id_profesor = p.id_profesor WHERE p.id_profesor IS NULL ORDER BY s.nombre;
 
 -- 6. Departments with no subjects in any course
-SELECT DISTINCT d.nombre AS departamento FROM departamento d LEFT JOIN profesor p ON d.id = p.id_departamento LEFT JOIN asignatura s ON p.id_profesor = s.id_profesor LEFT JOIN alumno_se_matricula_asignatura a ON s.id = a.id_asignatura LEFT JOIN curso_escolar c ON a.id_curso_escolar = c.id WHERE c.id IS NULL ORDER BY d.nombre;
+SELECT DISTINCT d.nombre AS departamento FROM departamento d LEFT JOIN profesor p ON d.id = p.id_departamento LEFT JOIN asignatura sub ON p.id_profesor = sub.id_profesor LEFT JOIN alumno_se_matricula_asignatura stu ON sub.id = stu.id_asignatura LEFT JOIN curso_escolar c ON stu.id_curso_escolar = c.id WHERE c.id IS NULL ORDER BY d.nombre;
 
 -- -----------------------------------------------------
 -- Summary queries
@@ -338,7 +338,7 @@ SELECT g.nombre AS grado, COUNT(s.id) AS numero_asignaturas FROM grado g LEFT JO
 SELECT g.nombre AS grado, s.tipo AS tipo_asignatura, SUM(s.creditos) AS numero_asignaturas FROM grado g JOIN asignatura s ON g.id = s.id_grado GROUP BY g.id, s.tipo ORDER BY g.nombre, s.tipo DESC;
 
 -- 8. Number of students per course
-SELECT c.anyo_inicio AS año_inicio, COUNT(a.id_alumno) AS numero_estudiantes FROM curso_escolar c LEFT JOIN alumno_se_matricula_asignatura a ON c.id = a.id_curso_escolar GROUP BY c.anyo_inicio ORDER BY c.anyo_inicio;
+SELECT c.anyo_inicio AS año_inicio, COUNT(s.id_alumno) AS numero_estudiantes FROM curso_escolar c LEFT JOIN alumno_se_matricula_asignatura s ON c.id = s.id_curso_escolar GROUP BY c.anyo_inicio ORDER BY c.anyo_inicio;
 
 -- 9. Professors ans their subjects
 SELECT per.id, per.nombre, per.apellido1, per.apellido2, COUNT(s.id) AS numero_asignaturas FROM profesor pro LEFT JOIN asignatura s ON pro.id_profesor = s.id_profesor JOIN persona per ON pro.id_profesor = per.id GROUP BY per.id ORDER BY per.id;
